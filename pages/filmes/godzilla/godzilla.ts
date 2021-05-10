b6c60500401e8221bc0deee25b548b2e5a284384
ronanzenatti <ronanzenatti@gmail.com>
2021-05-10 09:05:35 -0300

    2 files added

---
+++ pages/filmes/godzilla/godzilla.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-godzilla',
+  templateUrl: 'godzilla.html'
+})
+export class GodzillaPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
