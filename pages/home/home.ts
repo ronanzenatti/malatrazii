f620106149cd2d2de96dcf3b51ab53e450fa8b8e
ronanzenatti <ronanzenatti@gmail.com>
2021-05-10 09:46:35 -0300

    1 file modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -2,17 +2,15 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { GodzillaPage } from '../filmes/godzilla/godzilla';
+import { LigaJusticaPage } from '../filmes/liga-justica/liga-justica';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   paginaGodzilla = GodzillaPage;
+  paginaLigaJustica = LigaJusticaPage;
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  constructor(public navCtrl: NavController) {}
 }
